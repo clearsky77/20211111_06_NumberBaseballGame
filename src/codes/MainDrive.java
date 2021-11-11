5ae2098d1d731ec033502f2281dc9202a4029616
clearsky77 <clearsky3264@gmail.com>
2021-11-11 16:54:08 +0900

    검사 통과 문제 숫자로 선정 + 출제된 문제 출력

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -7,13 +7,42 @@ public class MainDrive {
 		int[] cpuNumbers = new int[3];
 		
 		//각 칸에 하나씩 (1~9) 랜덤 숫자를 채우기.
-
 		for (int i=0 ; i < cpuNumbers.length ; i++) {
 			//써도 되는 숫자가 나올때까지 무한 반복.
-			while (true) {
-				// 써도 되는 숫자면, cpuNumber의 위치에 맞게 집어넣고, 다음숫자로 넘어가자.
+			while (true) { // 써도 되는 숫자면, cpuNumber의 위치에 맞게 집어넣고, 다음숫자로 넘어가자.
+				// 1~9 중 하나를 랜덤으로 추출.
+				int randomNum = (int)  (Math.random() * 9 + 1);
+
+				// 중복 검사.  cpuNumbers에 같은 값이 있나? 확인.
+				boolean isDuplOk = true;
+				for (int cpuNum  : cpuNumbers) {
+					if ( randomNum  ==  cpuNum) { // 중복 숫자 발견! => 사용하면 안됨!
+						
+						isDuplOk = false;
+					}
+				}
+				
+				if (isDuplOk) {
+
+					cpuNumbers[i] = randomNum;
+
+//					무한 반복 탈출
+					break;
+
+				}
+				
 			}
 		}
+		
+//		나온 문제의 값을 출력
+		for(int cpuNum : cpuNumbers) {
+			System.out.println("문제 숫자: "+ cpuNum);
+		}
+		
+		
+		
+		
+		
 		
 	}
 
